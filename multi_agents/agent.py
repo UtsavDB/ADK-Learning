4a361fa881d40187a865e909537f7e97c02ca390
utsav verma <utsav.verma@aristocrat.com>
2026-03-16 17:25:51 +0530

    tool call successful

--- multi_agents/agent.py
+++ multi_agents/agent.py
@@ -1,10 +1,58 @@
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 
-AGENT_MODEL = "ollama/gemma3:1b"
+AGENT_MODEL = "gemini-3-flash-preview"
+
+
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city (e.g., "New York", "London", "Tokyo").
+
+    Returns:
+        dict: A dictionary containing the weather information.
+              Includes a 'status' key ('success' or 'error').
+              If 'success', includes a 'report' key with weather details.
+              If 'error', includes an 'error_message' key.
+              GOOGLE_API_KEY="AIzaSyDd0-TVzNI8EGsF6U84tu51qhzHaMe6A60"
+                GOOGLE_GENAI_USE_VERTEXAI=FALSE
+    """
+    print(f"--- Tool: get_weather called for city: {city} ---")  # Log tool execution
+    city_normalized = city.lower().replace(" ", "")  # Basic normalization
+
+    # Mock weather data
+    # api call
+    mock_weather_db = {
+        "newyork": {
+            "status": "success",
+            "report": "The weather in New York is sunny with a temperature of 25°C.",
+        },
+        "london": {
+            "status": "success",
+            "report": "It's cloudy in London with a temperature of 15°C.",
+        },
+        "tokyo": {
+            "status": "success",
+            "report": "Tokyo is experiencing light rain and a temperature of 18°C.",
+        },
+        "paris": {
+            "status": "success",
+            "report": "The weather in Paris is sunny with a temperature of 22°C.",
+        },
+    }
+
+    if city_normalized in mock_weather_db:
+        return mock_weather_db[city_normalized]
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Sorry, I don't have weather information for '{city}'.",
+        }
 
 root_agent = Agent(
     name="travel_planner_agent",
-    model=LiteLlm(model=AGENT_MODEL),
+    model=AGENT_MODEL,
     description="An agent that helps users plan their travel itineraries.",
-    instruction="You are a travel planner assistant. You can help users plan their travel itineraries by providing information about")
+    instruction="You are a travel planner assistant. You can help users plan their travel itineraries by providing information about",
+    tools=[get_weather])
